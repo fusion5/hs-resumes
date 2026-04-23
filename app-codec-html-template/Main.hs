f196a566b1cbd217c224faf9a613bbe31f11627c
Dragos I. <dragos.0x55@gmail.com>
2026-04-23 15:48:33 +0200

    remove trailing punctuation from text before it's passed to the template

--- app-codec-html-template/Main.hs
+++ app-codec-html-template/Main.hs
@@ -3,6 +3,7 @@ module Main (main) where
 import Control.Monad
 import Data.Aeson (Value (Object), eitherDecodeFileStrict, object, toJSON, (.=))
 import Data.Aeson.KeyMap qualified as KM
+import Data.Text
 import Data.Text.Lazy.IO qualified as TIO
 import Data.Time (getCurrentTime, utctDay)
 import Data.Time.Format.ISO8601 (iso8601Show)
@@ -12,8 +13,8 @@ import System.IO (hPutStrLn, stderr)
 import Text.Mustache
 
 data CLIArgs = CLIArgs
-  { cvInputFile :: FilePath
-  , cvTemplateFile :: FilePath
+  { cvInputFile :: FilePath,
+    cvTemplateFile :: FilePath
   }
   deriving (Show)
 
@@ -33,23 +34,23 @@ cliArgs =
 
 main :: IO ()
 main = do
-  CLIArgs{..} <- Opt.execParser cliOpts
+  CLIArgs {..} <- Opt.execParser cliOpts
   eitherDecodeFileStrict cvInputFile >>= \case
     Left err -> hPutStrLn stderr err
     Right (cv :: CV LLMPromptResultText) -> do
       template <- compileMustacheFile cvTemplateFile
       today <- utctDay <$> getCurrentTime
-      let
-        simpleCV = fmap getPromptResultText cv
-        (warnings, output) =
-          renderMustacheW template $ mergeObjects (toJSON simpleCV) (extraFields today)
+      let simpleCV = fmap (removeTrailingPunctuation . getPromptResultText) cv
+          (warnings, output) =
+            renderMustacheW template $ mergeObjects (toJSON simpleCV) (extraFields today)
       TIO.putStrLn output
-      unless (null warnings) $ do
+      unless (Prelude.null warnings) $ do
         hPutStrLn stderr "Mustache warnings:"
         forM_ warnings $ hPutStrLn stderr . displayMustacheWarning
- where
-  extraFields today = object ["today" .= iso8601Show today]
-  cliOpts = Opt.info (cliArgs Opt.<**> Opt.helper) Opt.fullDesc
+  where
+    removeTrailingPunctuation = dropWhileEnd (\c -> c `Prelude.elem` ['.', ',', ';'])
+    extraFields today = object ["today" .= iso8601Show today]
+    cliOpts = Opt.info (cliArgs Opt.<**> Opt.helper) Opt.fullDesc
 
 mergeObjects :: Value -> Value -> Value
 mergeObjects (Object a) (Object b) = Object (KM.union a b)
