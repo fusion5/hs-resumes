308d045778f32dcc03288bb25881ab436ba896e9
Dragos I. <dragos.0x55@gmail.com>
2026-04-27 10:44:47 +0200

    cover letter contents progress

--- src/DocumentTypes/CoverLetter.hs
+++ src/DocumentTypes/CoverLetter.hs
@@ -1,11 +1,47 @@
+{-# LANGUAGE DerivingVia #-}
+
 module DocumentTypes.CoverLetter (module DocumentTypes.CoverLetter) where
 
+import Autodocodec
+import Data.Aeson (FromJSON, ToJSON)
 import Data.Text
+import GHC.Generics
 
-data Content a = Paragraph a | List [Content a]
+data CoverLetterContent a = Paragraph a | List [CoverLetterContent a]
+  deriving (Eq, Show, Generic)
+  deriving (FromJSON, ToJSON) via (Autodocodec (CoverLetterContent a))
 
 data CoverLetter a = CoverLetter
   { currentLocation :: a,
     addressedTo :: Text,
-    paragraphs :: [Content a]
+    paragraphs :: CoverLetterContent a
   }
+  deriving (Eq, Show, Generic)
+  deriving (FromJSON, ToJSON) via (Autodocodec (CoverLetter a))
+
+instance (HasCodec a) => HasCodec (CoverLetterContent a) where
+  codec =
+    disjointMatchChoiceCodec paragraphCodec coverLetterListCodec decide
+    where
+      decide :: CoverLetterContent a -> Either (CoverLetterContent a) (CoverLetterContent a)
+      decide x@(Paragraph {}) = Left x
+      decide x@(List {}) = Right x
+
+      paragraphCodec =
+        object "Paragraph" $ Paragraph <$> requiredField "p" "Paragraph" .= unParagraph
+
+      coverLetterListCodec =
+        object "List" $ List <$> requiredField "l" "List" .= unList
+
+      unParagraph (Paragraph x) = x
+      unParagraph _ = error "logical error"
+      unList (List xs) = xs
+      unList _ = error "logical error"
+
+instance (HasCodec a) => HasCodec (CoverLetter a) where
+  codec =
+    object "CoverLetter" $
+      CoverLetter
+        <$> requiredField "location" "Location" .= currentLocation
+        <*> requiredField "to" "Addressed to" .= addressedTo
+        <*> requiredField "paragraphs" "Contents" .= paragraphs
